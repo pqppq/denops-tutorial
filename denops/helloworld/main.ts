3aeb15645a903417841776a1fd88bdd430cf56fd
yutatsu.oe <yutatsu0119@gmail.com>
2022-03-09 21:14:52 +0900

    add maze api

--- denops/helloworld/main.ts
+++ denops/helloworld/main.ts
@@ -1,18 +1,16 @@
 import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
-import { execute } from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts";
-import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
+import { Maze } from "https://deno.land/x/maze_generator@v0.4.0/mod.js";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    async echo(text: unknown): Promise<unknown> {
-      ensureString(text);
-      return await Promise.resolve(text);
+    async maze(): Promise<void> {
+      const maze = new Maze({}).generate();
+      const content = maze.getString();
+      console.log(content);
     },
   };
 
-  await execute(
-    denops,
-    // see :h com[mand][!]
-    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`
+  await denops.cmd(
+    `command! Maze call denops#request('${denops.name}', 'maze', [])`
   );
 }
